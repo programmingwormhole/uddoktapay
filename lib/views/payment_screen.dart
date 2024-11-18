79aa2c1e263057b9c94d141b4571ae364e609f66
Programming Wormhole <thennvvirus@gmail.com>
2024-11-18 08:13:08 -0800

    Version issue fixed

--- lib/views/payment_screen.dart
+++ lib/views/payment_screen.dart
@@ -7,7 +7,7 @@ import 'package:uddoktapay/core/services/api_services.dart';
 import 'package:uddoktapay/utils/config.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class PaymentScreen extends StatelessWidget {
+class PaymentScreen extends StatefulWidget {
   final String paymentURL;
 
   const PaymentScreen({
@@ -16,51 +16,70 @@ class PaymentScreen extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final controller = Get.put(PaymentController());
+  State<PaymentScreen> createState() => _PaymentScreenState();
+}
 
+class _PaymentScreenState extends State<PaymentScreen> {
+  late final WebViewController _webViewController;
+  final PaymentController controller = Get.put(PaymentController());
+
+  @override
+  void initState() {
+    super.initState();
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (NavigationRequest request) async {
+            if (request.url.startsWith(AppConfig.redirectURL)) {
+              controller.isPaymentVerifying.value = true;
+
+              Uri uri = Uri.parse(request.url);
+              String invoiceId = uri.queryParameters['invoice_id'] ?? '';
+
+              debugPrint('Invoice ID $invoiceId');
+
+              final response =
+                  await ApiServices.verifyPayment(invoiceId, context);
+
+              controller.isPaymentVerifying.value = false;
+
+              if (context.mounted) {
+                Navigator.pop(context, response);
+              }
+
+              return NavigationDecision.prevent;
+            }
+
+            if (request.url.startsWith(AppConfig.cancelURL)) {
+              controller.isPaymentVerifying.value = false;
+
+              if (context.mounted) {
+                Navigator.pop(context);
+              }
+
+              return NavigationDecision.prevent;
+            }
+
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.paymentURL));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Obx(
         () => Stack(
           children: [
             Column(
               children: [
-                const SizedBox(
-                  height: 20,
-                ),
+                const SizedBox(height: 20),
                 Expanded(
-                  child: WebView(
-                    initialUrl: paymentURL,
-                    javascriptMode: JavascriptMode.unrestricted,
-                    navigationDelegate: (NavigationRequest request) async {
-                      if (request.url.startsWith(AppConfig.redirectURL)) {
-                        controller.isPaymentVerifying.value = true;
-
-                        Uri uri = Uri.parse(request.url);
-                        String invoiceId =
-                            uri.queryParameters['invoice_id'] ?? '';
-
-                        debugPrint('Invoice ID $invoiceId');
-
-                        final response =
-                            await ApiServices.verifyPayment(invoiceId, context);
-
-                        controller.isPaymentVerifying.value = false;
-
-                        Navigator.pop(context, response);
-
-                        return NavigationDecision.prevent;
-                      }
-
-                      if (request.url.startsWith(AppConfig.cancelURL)) {
-                        controller.isPaymentVerifying.value = false;
-
-                        Navigator.pop(context);
-
-                        return NavigationDecision.prevent;
-                      }
-                      return NavigationDecision.navigate;
-                    },
+                  child: WebViewWidget(
+                    controller: _webViewController,
                   ),
                 ),
               ],
@@ -73,7 +92,7 @@ class PaymentScreen extends StatelessWidget {
                 child: const Center(
                   child: CircularProgressIndicator(),
                 ),
-              )
+              ),
             ]
           ],
         ),
